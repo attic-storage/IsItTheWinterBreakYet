cc7b19b0149431a4775b696554640072405baa63
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2019-10-22 20:10:43 +0200

    Authorize GET requests with CORS

--- fn.go
+++ fn.go
@@ -13,6 +13,15 @@ var (
 )
 
 func IsItTheWinterBreakYet(w http.ResponseWriter, r *http.Request) {
+	if  r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	fmt.Fprint(w, "{\"is_winter_break_yet\":\""+strconv.FormatBool(isWinterBreakYet())+"\", \"winter_break\": {\"start_time\":\""+startTime.Format(time.RFC3339)+"\", \"end_time\":\""+endTime.Format(time.RFC3339)+"\"}}")
 }
 
